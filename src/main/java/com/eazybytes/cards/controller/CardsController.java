3bf7f7bc7b9bb7f0f63121bc054142145677b437
SushantKhanal <dreamerking114@gmail.com>
2023-09-13 11:45:28 -0500

    @RequestHeader(eazybank-correlation-id) String correlationid added for tracing with gateway

--- src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -6,10 +6,7 @@ package com.eazybytes.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.eazybytes.cards.config.CardsServiceConfig;
 import com.eazybytes.cards.model.Cards;
@@ -35,7 +32,7 @@ public class CardsController {
 	CardsServiceConfig cardsConfig;
 
 	@PostMapping("/myCards")
-	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+	public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
 		if (cards != null) {
 			return cards;
