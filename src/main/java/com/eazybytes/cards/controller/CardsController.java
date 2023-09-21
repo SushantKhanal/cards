e5ef5b3adb85d800717ca188a9d78290042cc081
SushantKhanal <dreamerking114@gmail.com>
2023-09-20 23:38:09 -0500

    latest zipkin micrometer

--- src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -8,7 +8,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.cards.config.CardsServiceConfig;
 import com.eazybytes.cards.model.Cards;
@@ -26,7 +30,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardsController {
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
 
 	@Autowired
@@ -36,9 +40,10 @@ public class CardsController {
 	CardsServiceConfig cardsConfig;
 
 	@PostMapping("/myCards")
-	public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+	public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid,@RequestBody Customer customer) {
 		logger.info("getCardDetails() method started");
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		logger.info("getCardDetails() method ended");
 		if (cards != null) {
 			return cards;
 		} else {
